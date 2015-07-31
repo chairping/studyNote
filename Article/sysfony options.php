6a1d0751629d30b6b3ecd0f231d2af075da38032
chenping <chenping@273.cn>
2015-07-31 11:24:26 +0000

    xx

--- Article/sysfony options.php
+++ Article/sysfony options.php
@@ -7,7 +7,9 @@ $options = [
     'items' => isset($input['items']) ? $input['items'] : 10
 ];
 
-Here, we want to have an array with options that come from the user (for example, from a RESTful API). And if any of the required options is not present, we use default options. For this, we need to check if it¡¯s present and use the default value otherwise. All this boilerplate is difficult to read and very repetitive.
+Here, we want to have an array with options that come from the user (for example, from a RESTful API).
+And if any of the required options is not present, we use default options.
+For this, we need to check if it¡¯s present and use the default value otherwise. All this boilerplate is difficult to read and very repetitive.
 
 An alternative would be to use the array_replace function:
 $options = array_replace([
@@ -15,9 +17,11 @@ $options = array_replace([
     'items' => 10
 ], $input);
 
-The array_replace function replaces elements from the second argument into the first one, so if $input contains a different value for any of the options, it would be overwritten.
+The array_replace function replaces elements from the second argument into the first one,
+so if $input contains a different value for any of the options, it would be overwritten.
 
-While this solution is good enough, the OptionsResolver component provides a few interesting features. In addition to dealing with default values, it can validate and normalize the data.
+While this solution is good enough, the OptionsResolver component provides a few interesting features.
+In addition to dealing with default values, it can validate and normalize the data.
 Simple example
 
 The previous example using OptionsResolver would look like this:
@@ -32,22 +36,26 @@ $resolver->setDefaults([
 
 $options = $resolver->resolve($input);
 
-Once executed, $options will contain a plain array with two elements: ¡®page¡¯ and ¡®items¡¯. If $input contains values for any of these values, they will be overwritten. Let¡¯s see a few examples:
+Once executed, $options will contain a plain array with two elements: ¡®page¡¯ and ¡®items¡¯.
+If $input contains values for any of these values, they will be overwritten. Let¡¯s see a few examples:
 $input = []; // 'page' => 1, 'items' => 10
 $input = ['page' => 2]; // 'page' => 2, 'items' => 10
 $input = ['page' => 2, 'items' => 20]; // 'page' => 2, 'items' => 20
 $input = ['other' => 5]; // UndefinedOptionsException
 
-That¡¯s right! If we try to use a value other than the ones defined, it throws an exception. This would be the first advantage of using the component over array_replace, but there are more.
+That¡¯s right! If we try to use a value other than the ones defined, it throws an exception.
+This would be the first advantage of using the component over array_replace, but there are more.
 
-It is also possible to use a closure to set default parameters in case they depend on something else. We can even set default parameters based on other parameters. For example, let¡¯s add a third parameter, ¡®order¡¯, and define the order randomly. You know¡­ improving UX :)
+It is also possible to use a closure to set default parameters in case they depend on something else.
+We can even set default parameters based on other parameters. For example, let¡¯s add a third parameter, ¡®order¡¯, and define the order randomly. You know¡­ improving UX :)
 $resolver->setDefault('order', function (Options $options) {
     $orders = ['asc', 'desc'];
 
     return $orders[rand(0, 1)];
 });
 
-As you can see, the function receives an Options object, so we can use it to generate default values based on other parameters. Imagine that we have also a ¡®order_by¡¯ parameter, so we could set the order based on this:
+As you can see, the function receives an Options object, so we can use it to generate default values based on other parameters.
+Imagine that we have also a ¡®order_by¡¯ parameter, so we could set the order based on this:
 $resolver->setDefault('order', function (Options $options) {
     if ('creation_date' === $options['order_by']) {
         return 'desc';
@@ -57,15 +65,18 @@ $resolver->setDefault('order', function (Options $options) {
 });
 Validation
 
-We may want to validate input data, something more than necessary when dealing with user-provided data. The component provides some ways to validate data by type or by value using the methods setAllowedTypes() and setAllowedValues().
+We may want to validate input data, something more than necessary when dealing with user-provided data.
+The component provides some ways to validate data by type or by value using the methods setAllowedTypes() and setAllowedValues().
 
-With setAllowedTypes(), it is possible to restrict the value of the option to the given types. For example, to restrict ¡®page¡¯ values to integer values:
+With setAllowedTypes(), it is possible to restrict the value of the option to the given types.
+For example, to restrict ¡®page¡¯ values to integer values:
 $resolver->setAllowedTypes('page', 'int');
 
 It is also possible to allow more than one type:
 $resolver->setAllowedTypes('page', ['int', 'float']);
 
-What if we want to constrain the range of values that can be used? Using the method setAllowedValues() we can define a set of allowed values. For example, to limit the possible values of ¡®items¡¯ to 10, 20 or 40:
+What if we want to constrain the range of values that can be used? Using the method setAllowedValues() we can define a set of allowed values.
+For example, to limit the possible values of ¡®items¡¯ to 10, 20 or 40:
 $resolver->setAllowedValues('items', [10, 20, 40]);
 
 But setAllowedValues() is much more powerful, as it can receive a Closure. In the following example, we can validate that ¡®page¡¯ is between 1 and 10:
@@ -73,7 +84,8 @@ $resolver->setAllowedValues('page', function($value) {
     return $value >= 1 && $value <=10;
 });
 
-If you read the post about the Validator component, you know that there is already a constraint to define ranges. Integrating the validator component is really easy:
+If you read the post about the Validator component, you know that there is already a constraint to define ranges.
+Integrating the validator component is really easy:
 use Symfony\Component\Validator\Validation;
 use Symfony\Component\Validator\Constraints\Range;
 
@@ -90,7 +102,9 @@ $resolver->setAllowedValues('items', function($value) {
 });
 Normalization
 
-Finally, we can also normalize data before using the options with the setNormalizer() method. It accepts a Closure that receives the value and the rest of options. For example, we could normalize the ¡®page¡¯ option in case that the value provided by the user is over the maximum number of pages:
+Finally, we can also normalize data before using the options with the setNormalizer() method.
+It accepts a Closure that receives the value and the rest of options.
+For example, we could normalize the ¡®page¡¯ option in case that the value provided by the user is over the maximum number of pages:
 $resolver->setNormalizer('page', function ($options, $value) use ($maxPage) {
     if ($value > $maxPage) {
         $value = $maxPage;
---
+++ lib/Arr.php
@@ -0,0 +1,233 @@
+<?php
+
+namespace Carbon;
+
+class Arr
+{
+//  $array = [
+//      '1' => 'a',
+//      '2' => ['3' => 'c']
+//  ];
+//
+//  $a = Arr::get($array, '2.3');
+//  var_dump($a); out  'c'
+    public static function get($array, $key, $default = null)
+    {
+        if (is_null($key)) return $array;
+
+        if (isset($array[$key])) return $array[$key];
+
+        foreach (explode('.', $key) as $segment)
+        {
+            if ( ! is_array($array) || ! array_key_exists($segment, $array))
+            {
+                return $default;
+            }
+
+            $array = $array[$segment];
+        }
+
+        return $array;
+    }
+
+
+    public static function divide($array)
+    {
+        return array(array_keys($array), array_values($array));
+    }
+
+//  $array = [
+//      '1' => 'a',
+//      '2' => ['3' => 'c']
+//  ];
+//  $a = Arr::set($array, '1.2.3', 'cc');
+//  var_dump($a);
+//  var_dump($array);
+//array (size=2)
+//    1 =>
+//        array (size=1)
+//            2 =>
+//                array (size=1)
+//                    3 => string 'cc' (length=2)
+//    2 =>
+//        array (size=1)
+//            3 => string
+    public static function set(&$array, $key, $value)
+    {
+        if (is_null($key)) return $array = $value;
+
+        $keys = explode('.', $key);
+
+        while (count($keys) > 1)
+        {
+            $key = array_shift($keys);
+
+            // If the key doesn't exist at this depth, we will just create an empty array
+            // to hold the next value, allowing us to create the arrays to hold final
+            // values at the correct depth. Then we'll keep digging into the array.
+            if ( ! isset($array[$key]) || ! is_array($array[$key]))
+            {
+                $array[$key] = array();
+            }
+
+            $array =& $array[$key];
+        }
+
+        $array[array_shift($keys)] = $value;
+
+        return $array;
+    }
+    // 功能上与array_map 相似 但是可以控制key
+    public static function build($array, Closure $callback)
+    {
+        $results = array();
+
+        foreach ($array as $key => $value)
+        {
+            list($innerKey, $innerValue) = call_user_func($callback, $key, $value);
+
+            $results[$innerKey] = $innerValue;
+        }
+
+        return $results;
+    }
+
+//array (size=2)
+//0 => string 'a' (length=1)
+//'2.3' => string 'c' (length=1)
+    public static function dot($array, $prepend = '')
+    {
+        $results = array();
+
+        foreach ($array as $key => $value)
+        {
+            if (is_array($value))
+            {
+                $results = array_merge($results, static::dot($value, $prepend.$key.'.'));
+            }
+            else
+            {
+                $results[$prepend.$key] = $value;
+            }
+        }
+
+        return $results;
+    }
+
+    // 返回差集
+    public static function except($array, $keys)
+    {
+        return array_diff_key($array, array_flip((array) $keys));
+    }
+
+//$array = [
+//'1' => ['a' => array('xxx'=> 3)],
+//'2' => ['a' => 'c'],
+//'3' => ['b' => 'c']
+//];
+//$a = Arr::fetch($array, 'a.xxx');
+//array (size=1)
+//0 => int 3   返回指定的值
+    public static function fetch($array, $key)
+    {
+        foreach (explode('.', $key) as $segment)
+        {
+            $results = array();
+
+            foreach ($array as $value)
+            {
+                if (array_key_exists($segment, $value = (array) $value))
+                {
+                    $results[] = $value[$segment];
+                }
+            }
+
+            $array = array_values($results);//var_dump($array);
+        }
+
+        return array_values($results);
+    }
+    //  返回符合条件的第一个值
+    public static function first($array, $callback, $default = null)
+    {
+        foreach ($array as $key => $value)
+        {
+            if (call_user_func($callback, $key, $value)) return $value;
+        }
+
+        return value($default);
+    }
+
+    // 返回符合条件的最后一个值
+    public static function last($array, $callback, $default = null)
+    {
+        return static::first(array_reverse($array), $callback, $default);
+    }
+
+    // 把多级数组化成单一数组
+    public static function flatten($array)
+    {
+        $return = array();
+
+        array_walk_recursive($array, function($x) use (&$return) { $return[] = $x; });
+
+        return $return;
+    }
+    // 删除指定层级的值
+    public static function forget(&$array, $keys)
+    {
+        $original =& $array;
+
+        foreach ((array) $keys as $key)
+        {
+            $parts = explode('.', $key);
+
+            while (count($parts) > 1)
+            {
+                $part = array_shift($parts);
+
+                if (isset($array[$part]) && is_array($array[$part]))
+                {
+                    $array =& $array[$part];
+                }
+            }
+
+            unset($array[array_shift($parts)]);
+
+            // clean up after each pass
+            $array =& $original;
+        }
+    }
+    // 返回键名的交集
+    public static function only($array, $keys)
+    {
+        return array_intersect_key($array, array_flip((array) $keys));
+    }
+
+    public static function pluck($array, $value, $key = null)
+    {
+        $results = array();
+
+        foreach ($array as $item)
+        {
+            $itemValue = is_object($item) ? $item->{$value} : $item[$value];
+
+            // If the key is "null", we will just append the value to the array and keep
+            // looping. Otherwise we will key the array using the value of the key we
+            // received from the developer. Then we'll return the final array form.
+            if (is_null($key))
+            {
+                $results[] = $itemValue;
+            }
+            else
+            {
+                $itemKey = is_object($item) ? $item->{$key} : $item[$key];
+
+                $results[$itemKey] = $itemValue;
+            }
+        }
+
+        return $results;
+    }
+
+}
