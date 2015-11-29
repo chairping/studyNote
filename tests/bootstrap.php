09cf654ed34063813244c78e796fe6169e8610e2
chenping <381871149@qq.com>
2015-11-29 20:55:18 +0800

    udpate

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG',True);
-// 定义应用目录
-define('APP_PATH', __DIR__ . '/../Application/');
-try{
-    require __DIR__ . '/../ThinkPHP/ThinkPHP.php';
-}catch (\Think\Exception $e) {
-}
